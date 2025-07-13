ad306f9a09a7c33effa441786a10c9b5c2582977
Murthy Udupa <ugmurthy@gmail.com>
2025-07-13 15:24:38 -0700

    icons are consistent in all routes

--- jobs-client/src/pages/jobs/JobsPage.tsx
+++ jobs-client/src/pages/jobs/JobsPage.tsx
@@ -497,19 +497,8 @@ export default function JobsPage() {
                         <td className="px-6 py-4 text-center whitespace-nowrap">
                           <div className="flex justify-center space-x-2">
                             <Link
-                              to={`/jobs/${job.id}`}
-                              className="p-1 text-blue-600 bg-blue-100 rounded hover:bg-blue-200 dark:bg-blue-900/30 dark:hover:bg-blue-800/50"
-                              title="View"
-                            >
-                              <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                              </svg>
-                            </Link>
-                            
-                            <Link
                               to={`/jobs/edit/${job.id}`}
-                              className="p-1 text-yellow-600 bg-yellow-100 rounded hover:bg-yellow-200 dark:bg-yellow-900/30 dark:hover:bg-yellow-800/50"
+                              className="p-1 text-blue-600 bg-blue-100 rounded hover:bg-blue-200 dark:bg-blue-900/30 dark:hover:bg-blue-800/50"
                               title="Edit"
                             >
                               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
